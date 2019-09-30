383b48483aa2d02fccb5f3b2b266e9f0850e9bd2
Derek <legato_5@yahoo.com>
2019-09-29 19:41:33 -0700

    added styling to cards

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,15 +1,26 @@
 import React, { useState, useEffect } from "react";
 import CharacterList from './CharacterList.js';
+import {
+    Card, CardText, CardBody,
+    CardTitle, CardSubtitle, Row, Col
+} from 'reactstrap';
 
 export default function CharacterCard(props) {
 
     return(
-        <div>
-            <h2>Character: {props.name}</h2>
-            <p>Birth: {props.birth}</p>
-            <p>Gender: {props.gender}</p>
-            <p>Eye Color: {props.eye_color}</p>
-            <p>Species: {props.species}</p>
-        </div>
+        <Row>
+            <Col sm="6" md={{ size: 6, offset: 3}}>
+            <Card inverse style={{ backgroundColor: '#333', borderColor: '#fff' }}>
+              <CardBody>
+                <CardTitle>Character: {props.name}</CardTitle>
+                <CardSubtitle>Birth: {props.birth}</CardSubtitle>
+                <CardText>Gender: {props.gender}</CardText>
+                <CardText>Eye Color: {props.eye_color}</CardText>
+              </CardBody>
+            </Card>
+            <br />
+          </Col>
+        </Row>
+            
     )
 }
--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import CharacterCard from './CharacterCard.js';
 import axios from "axios";
+import {
+    Card, CardText, CardBody,
+    CardTitle, CardSubtitle, Row, Col
+} from 'reactstrap';
 
 export default function CharacterList(props) {
     const [char, setChar] = useState([]);
@@ -19,14 +23,16 @@ export default function CharacterList(props) {
     return (
         <div>
             {char.map((item, index) => {
-                return <CharacterCard 
-                    name = {item.name}
-                    birth = {item.birth_year}
-                    gender = {item.gender}
-                    eye_color = {item.eye_color}
-                    species = {item.species}
-                    key = {index}
-            />
+                return (
+                    <CharacterCard 
+                        name = {item.name}
+                        birth = {item.birth_year}
+                        gender = {item.gender}
+                        eye_color = {item.eye_color}
+                        key = {index}
+                        
+                />
+                )
             })}
         </div>
     )
